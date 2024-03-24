47f84d393518eeb169fa9ce27ded4fbaf204afbb
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 13:56:54 +0700

    commit Ex03

--- 65030179_Lab07/Lab07_Ex03/Program.cs
+++ 65030179_Lab07/Lab07_Ex03/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MyClass
+{
+    private double My_PI;
+
+    public MyClass()
+    {
+        My_PI = Math.PI;
+    }
+
+    public double MyValue
+    {
+        get
+        {
+            return My_PI;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ตัวอย่างการใช้งาน MyClass
+        MyClass mc = new MyClass();
+        Console.WriteLine($"Value of Pi: {mc.MyValue}");
+    }
+}
