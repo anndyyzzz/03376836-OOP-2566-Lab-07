4a7b9e000e4acfbe1a17c87223ce1af1ea25c60e
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 13:46:45 +0700

    commit Ex02

--- 65030179_Lab07/Lab07_Ex02/Program.cs
+++ 65030179_Lab07/Lab07_Ex02/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MyClass mc = new MyClass();
+mc.MyValue = 50;
+System.Console.WriteLine($"mc.MyValue = {mc.MyValue}");
+mc.MyValue = 150;
+System.Console.WriteLine($"mc.MyValue = {mc.MyValue}");
+
+class MyClass
+{
+    private int StoredPercentValue;
+    public int MyValue
+    {
+        set
+        {   
+            if(value > 100)
+                StoredPercentValue = 100;
+                else
+                StoredPercentValue = value;
+        }
+        get
+        {
+            return StoredPercentValue;
+        }
+    }
+}
