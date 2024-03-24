e38bd87a95c08373f7d8d4f84a9e79633a463c65
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 14:04:38 +0700

    commit Ex04

--- 65030179_Lab07/Lab07_Ex04/Program.cs
+++ 65030179_Lab07/Lab07_Ex04/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MyClass
+{
+    private int StoredValue;
+    public int MyValue
+    {
+        get
+        {
+            return StoredValue;
+        }
+        set
+        {
+            StoredValue = value;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass mc = new MyClass();
+        mc.MyValue = 50;
+        Console.WriteLine($"My value  = {mc.MyValue}");
+    }
+}
