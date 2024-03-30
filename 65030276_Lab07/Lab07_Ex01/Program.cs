9f7360fb5f3d100c3a9e8dec928dc9dd6d24eab8
anndyyz <anda2546r@gmail.com>
2024-03-30 20:37:12 +0700

    ex1

---
+++ 65030276_Lab07/Lab07_Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿var c1 = new Circle();
+c1.Radius = 3.0f;
+c1.Info();
+c1.Diameter = 8.0;
+c1.Info();
+
+class Circle
+{
+    private double radius;
+    public double Radius 
+    { 
+        get{return radius;} 
+        set{radius = value;} 
+    }
+    public double Diameter
+    {
+        get{return radius * 2;} 
+        set{radius = value / 2.0;}
+    }
+
+    public void Info()
+    {
+        System.Console.WriteLine($"My circle has radius of {radius:F4} and diameter of {Diameter:F4} unit");
+    }
+}
